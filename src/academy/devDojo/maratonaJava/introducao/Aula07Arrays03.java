329ed9bbee862ec4c040c988e8c2619d7eec3255
LuisMIguelFurlanettoSousa <lfurlanettosousa@gmail.com>
2025-03-28 16:44:06 -0300

    arrays multidimensionais

--- src/academy/devDojo/maratonaJava/introducao/Aula07Arrays03.java
+++ src/academy/devDojo/maratonaJava/introducao/Aula07Arrays03.java
@@ -1,4 +1,13 @@
 package academy.devDojo.maratonaJava.introducao;
 
 public class Aula07Arrays03 {
+    public static void main(String[] args) {
+        int[] numeros = new int[3];
+        int[] numeros2 = {1, 8, 9};
+        int[] numeros3 = new int[] {1, 8, 9};
+
+        for (int num : numeros2) {
+            System.out.println(num);
+        }
+    }
 }
---
+++ src/academy/devDojo/maratonaJava/introducao/Aula08ArraysMultidimencionais01.java
@@ -0,0 +1,25 @@
+package academy.devDojo.maratonaJava.introducao;
+
+public class Aula08ArraysMultidimencionais01 {
+    public static void main(String[] args) {
+        int[][] numeros = new int[3][3];
+
+        numeros[0][0] = 3;
+        numeros[0][1] = 3;
+        numeros[0][2] = 3;
+
+        numeros[1][0] = 1;
+        numeros[1][1] = 1;
+        numeros[1][2] = 1;
+
+        numeros[2][0] = 4;
+        numeros[2][1] = 5;
+        numeros[2][2] = 6;
+
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros[i].length; j++) {
+                System.out.println(numeros[i][j]);
+            }
+        }
+    }
+}
