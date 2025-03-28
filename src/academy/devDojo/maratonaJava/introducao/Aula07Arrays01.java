75299d8eeb9380b9cbe2a69596be507c5e1a51ee
LuisMIguelFurlanettoSousa <lfurlanettosousa@gmail.com>
2025-03-28 00:29:32 -0300

    introducao a arrays

---
+++ src/academy/devDojo/maratonaJava/introducao/Aula07Arrays01.java
@@ -0,0 +1,13 @@
+package academy.devDojo.maratonaJava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+
+        idades[0] = 1;
+        idades[1] = 34;
+        idades[2] = 90;
+
+        System.out.println(idades[1]);
+    }
+}
