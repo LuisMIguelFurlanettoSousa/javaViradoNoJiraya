4c336a5a04b4bec6e650d55167a6fe4997738006
LuisMIguelFurlanettoSousa <lfurlanettosousa@gmail.com>
2025-04-12 08:30:18 -0300

    fazendo teste

--- src/academy/devDojo/maratonaJava/Javacore/Bintroducaometodos/test/PessoaMyTeste01.java
+++ src/academy/devDojo/maratonaJava/Javacore/Bintroducaometodos/test/PessoaMyTeste01.java
@@ -4,8 +4,23 @@ import academy.devDojo.maratonaJava.Javacore.Bintroducaometodos.dominio.teste.Ex
 import academy.devDojo.maratonaJava.Javacore.Bintroducaometodos.dominio.teste.GeraPessoa;
 
 public class PessoaMyTeste01 {
-    GeraPessoa pessoa = new GeraPessoa();
-    ExibePessoa exibePessoa = new ExibePessoa();
+    public static void main(String[] args) {
+        GeraPessoa geraPessoa = new GeraPessoa();
+        GeraPessoa geraPessoa1 = new GeraPessoa();
+        ExibePessoa exibePessoa = new ExibePessoa();
 
+        geraPessoa.setNome("Luis");
+        geraPessoa.setIdade(18);
+        geraPessoa.setSexo('M');
+        geraPessoa.setVivo(true);
 
+        geraPessoa1.setNome("Julia");
+        geraPessoa1.setIdade(17);
+        geraPessoa1.setSexo('F');
+        geraPessoa1.setVivo(true);
+
+        System.out.println(geraPessoa.getNome());
+        exibePessoa.exibe(geraPessoa);
+        exibePessoa.exibe(geraPessoa1);
+    }
 }
