bc1a4b82c299a2a551d394a4dac37e042549adf2
LuisMIguelFurlanettoSousa <lfurlanettosousa@gmail.com>
2025-05-07 18:45:20 -0300

    Classe especial enum

--- src/academy/devDojo/maratonaJava/Javacore/Kenum/dominio/Cliente.java
+++ src/academy/devDojo/maratonaJava/Javacore/Kenum/dominio/Cliente.java
@@ -1,19 +1,28 @@
 package academy.devDojo.maratonaJava.Javacore.Kenum.dominio;
 
 public class Cliente {
+    public enum TipoPagamento {
+        DEBITO,
+        CREDITO
+    }
+
     private String nome;
     private TipoCliente tipoCliente;
+    private TipoPagamento tipoPagamento;
 
-    public Cliente(String nome, TipoCliente tipoCliente) {
+    public Cliente(String nome, TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
         this.nome = nome;
         this.tipoCliente = tipoCliente;
+        this.tipoPagamento = tipoPagamento;
     }
 
     @Override
     public String toString() {
         return "Cliente{" +
                 "nome='" + nome + '\'' +
-                ", tipo='" + tipoCliente + '\'' +
+                ", tipoCliente=" + tipoCliente.getNomeRelatorio() +
+                ", tipoClienteInt=" + tipoCliente.VALOR +
+                ", tipoPagamento=" + tipoPagamento +
                 '}';
     }
 
--- src/academy/devDojo/maratonaJava/Javacore/Kenum/teste/clienteTeste01.java
+++ src/academy/devDojo/maratonaJava/Javacore/Kenum/teste/clienteTeste01.java
@@ -1,14 +1,16 @@
 package academy.devDojo.maratonaJava.Javacore.Kenum.teste;
 
 import academy.devDojo.maratonaJava.Javacore.Kenum.dominio.Cliente;
+import academy.devDojo.maratonaJava.Javacore.Kenum.dominio.Cliente.TipoPagamento;
 import academy.devDojo.maratonaJava.Javacore.Kenum.dominio.TipoCliente;
 
 public class clienteTeste01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("um ali", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("O certinn que paga bunitin", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("um ali", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("O certinn que paga bunitin", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+
     }
 }
